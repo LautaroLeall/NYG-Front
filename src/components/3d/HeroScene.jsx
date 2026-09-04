a9b92eea7bfd49656299bfd41b1e2c108191c753
LautaroLeall <lautaroleal4@gmail.com>
2026-09-04 12:21:18 -0300

    style(ui): ajustes menores y formateo en componente HeroScene 3D

--- src/components/3d/HeroScene.jsx
+++ src/components/3d/HeroScene.jsx
@@ -1,5 +1,5 @@
-import { Canvas } from '@react-three/fiber';
-import { Sparkles } from '@react-three/drei';
+import { Canvas } from "@react-three/fiber";
+import { Sparkles } from "@react-three/drei";
 
 const HeroScene = () => {
   return (
@@ -7,17 +7,16 @@ const HeroScene = () => {
       {/* Asegurar que el fondo del Canvas sea transparente (alpha: true) */}
       <Canvas camera={{ position: [0, 0, 8], fov: 45 }} gl={{ alpha: true }}>
         <ambientLight intensity={0.5} />
-        
+
         {/* Partículas doradas flotantes simulando gloria/historia cayendo sobre la foto */}
-        <Sparkles 
-          count={300} 
-          scale={15} 
-          size={3} 
-          speed={0.3} 
-          color="#D4AF37" 
+        <Sparkles
+          count={300}
+          scale={15}
+          size={3}
+          speed={0.3}
+          color="#D4AF37"
           opacity={0.8}
         />
-        
       </Canvas>
     </div>
   );
